c0c63e56daa6cb2faf35018f84e969c6c4d33fc9
wanderleylb <wanderleylb@gmail.com>
2026-08-12 19:00:06 -0300

    salao.js
    Added comments for better code clarity and understanding.

--- js/salao.js
+++ js/salao.js
@@ -1,7 +1,7 @@
 
 // =========================================================
 // FUNÇÃO DE CADASTRO DO AGENDAMENTO
-// 
+// ========================================================= 
 
 function cadastrarAgendamento() {
 
@@ -17,22 +17,58 @@ function cadastrarAgendamento() {
     let horario = document.getElementById("horario").value;
 
     // ---------------------------------------------------------
-    // RECUPERA OS SERVIÇOS
+    // RECUPERA OS SERVIÇOS OS SERVIÇOS SELECINADOS PELO USUÁRIO
     // ---------------------------------------------------------
+	/* querySelectorAll procura no HTML todos os elementos
+	   que possuem a classe "serv" e que estão marcados (:checked).
+	   Exemplo: se o aluno marcou "Corte" e "Barba",
+	   os dois elementos serão armazenado na variável "serviços".
+	*/
     let servicos = document.querySelectorAll(".serv:checked");
-    
-    // Cria uma lista vazia para armazenar os serviços
+    // ----------------------------------------------------------
+    // CRIA UMA LISTA VAZIA PARA ARMAZENAR OS SERVIÇOS ESCOLHIDOS
+	// ----------------------------------------------------------
+	
+	/* Cria uma array (lista) vazio.
+	   Ele será usado para armazenar somente os valores
+	   dos serviços que foram selecionados pelo usuário.
+	*/
 
     let listaServicos = [];
-
-    // Percorre os serviços selecionados
-
+	
+    // ----------------------------------------------------------
+    // PERCORRE TODOS OS SERVIÇOS SELECIONADOS
+    // ----------------------------------------------------------
+	
+    /*  forEach() percorre um por um os serviços encontrados.
+	    A variável "serviços" representa o serviço atual.
+		durante casa repetição do forEach().
+	*/
     servicos.forEach(function(servico) {
+		
+	/* push*() adiciona um novo item ao final do array.
+	   o ".value" pega o valor do serviço definido no HTML.
+	
+	*/	
 
         listaServicos.push(servico.value);
 
          // Adiciona o serviço à lista
     });
+	
+	// ---------------------------------------------------------
+    // SALVA OS DADOS NO LOCALSTORAGE
+    // ---------------------------------------------------------
+	/* Ao final, "listaServicos" terá somente os serviços
+	que foram marcados pelo usuário.
+	
+	 EXEMPLO:
+	 Se o usuário selecionar: Corte, Barba e Sombrancelha
+	 
+	 O array ficará assim:
+	 ["Corte", "Barba", "Sombrancelha"]
+	
+	*/
 
     // ---------------------------------------------------------
     // SALVA OS DADOS NO LOCALSTORAGE
